4f47412491160dea921317f89c0f675b21ffa24e
Chris Brooks <chris@chrisbrooks.org>
2019-11-21 13:17:11 -0800

    Singular cat lookup is working yall

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
 api_app = Flask(__name__)
 
+cats = ['Bengal', 'Persian', 'Siamese']
+
 
 @api_app.route('/')
 def hello_world():
@@ -9,4 +11,9 @@ def hello_world():
 
 @api_app.route('/api/cat-breeds/')
 def cat_breeds_index():
-    return jsonify(['Bengal', 'Persian', 'Siamese'])
+    return jsonify(cats)
+
+
+@api_app.route('/api/cat-breeds/<int:id>')
+def cat_breed(id):
+    return jsonify(cats[id])
--- test_api.py
+++ test_api.py
@@ -38,3 +38,13 @@ def test_api_cat_breeds_three_breeds(client):
 def test_api_get_first_cat_breed_is_200(client):
     response = client.get('api/cat-breeds/0')
     assert response.status_code == 200
+
+
+def test_api_first_cat_shall_be_bengal(client):
+    response = client.get('api/cat-breeds/0')
+    assert str(response.json) == "Bengal"
+
+
+def test_api_second_cat_shall_be_persian(client):
+    response = client.get('api/cat-breeds/1')
+    assert str(response.json) == "Persian"
